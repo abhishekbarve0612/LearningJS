c41fd4b88cec80f3653cc319e76e20574127dbac
abhishekbarve0612 <barveabhishek.ab@gmail.com>
2021-01-26 11:43:47 +0530

    Completed Learning FlexBox

--- 24-01-2021/Bomberman/script.js
+++ 24-01-2021/Bomberman/script.js
@@ -2,37 +2,51 @@ const container = document.querySelector("#main-container");
 var array = new Array(36);
 var visited = new Array();
 var bombs = new Array();
+let score = 0;
 function randomArray(){
-  for (let i = 0; i < 10; i++) {
-    var num = Math.floor(Math.random() * (35 - 0) + 0);
+  for (let i = 0; i < 20; i++) {
+    var num = Math.floor(Math.random() * (80 - 0) + 0);
     array[num] = 1;
     bombs.push(num);
   }
 }
 
+function increaseScore(){
+  score++;
+  var gscore = document.getElementById("gameScore");
+  gscore.innerText = score;
+}
+
 function setContainer(){
-  for (let count = 0; count < 6*6; count++) {
+  for (let count = 0; count < 9*9; count++) {
     let div = document.createElement("div");
     div.setAttribute("data-index", count);
     div.setAttribute("class", "cell");
-    div.addEventListener("click", (e) => {
-      var ele = parseInt(e.target.getAttribute("data-index"));
+    div.id = "cell_"+count;
+    console.log(div);
+    div.addEventListener("click", fun(count));
+    container.appendChild(div);
+  }
+}
+
+function fun(count) {
+  var cellid = "cell_" + count;
+  console.log(cellid);
+  var element2 = document.getElementById(cellid);
+  var ele = parseInt(element2.getAttribute("data-index"));
       if (visited.includes(ele) == false){
         if (array[ele] == 1) {
           gameOver("You Lost!!!");
           return;
         }
-        e.target.classList.add("visited");
+        element.classList.add("visited");
+        increaseScore();
         visited.push(ele);
         if (visited.length > 10){
           gameOver("You Won!");
           return;
         }
       }
-        
-    });
-    container.appendChild(div);
-  }
 }
 
 
@@ -52,14 +66,13 @@ function revealBombs() {
     if (bombs.includes(num) == true){
       node.classList.add("bombed");
     }
-    node.addEventListener("click", (e) => {
-      alert("Game Over!!!!")
-    });
+    node.removeEventListener("click", fun(num));
   }
 }
 
 function startGame() {
   visited = [];
+  score = 0;
   array.fill(-1);
   randomArray();
   container.innerHTML = "";
